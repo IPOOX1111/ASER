e3ef094005f4ec53af29e2063275532b7df4150f
IPOOX1111 <155116618+IPOOX1111@users.noreply.github.com>
2026-04-23 13:06:38 +0100

    Delete voting-project directory

--- voting-project/db/initdb/primary_conf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-PGDATA="/var/lib/postgresql/data"
-
-echo "wal_level = replica" >> $PGDATA/postgresql.conf
-echo "max_wal_senders = 3" >> $PGDATA/postgresql.conf
-echo "hot_standby = on" >> $PGDATA/postgresql.conf
-
-echo "host replication voter 0.0.0.0/0 md5" >> $PGDATA/pg_hba.conf
